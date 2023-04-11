4fd09ea9069f8a7a7d8366a73ee3d9474b186654
ranpro <srizym@gmail.com>
2023-04-11 18:28:39 +0800

    feat: 添加代理支持

--- app/Modules/Service/OpenAI/OpenAIServiceProvider.php
+++ app/Modules/Service/OpenAI/OpenAIServiceProvider.php
@@ -10,7 +10,10 @@ class OpenAIServiceProvider extends ServiceProvider
     public function register()
     {
         $this->app->bind(OpenAI\Client::class, function () {
-            return OpenAI::client(config('openai.api_key'));
+            return OpenAI::factory()
+                ->withBaseUri(config('openai.endpoint'))
+                ->withApiKey(config('openai.api_key'))
+                ->make();
         });
     }
 }
