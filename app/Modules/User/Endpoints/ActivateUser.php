b260d08e9e74e7cd54b2ea8e6f9fb6e298ea2b2d
ranpro <srizym@gmail.com>
2023-04-13 19:54:39 +0800

    feat: 邀请码统一小写处理

--- app/Modules/User/Endpoints/ActivateUser.php
+++ app/Modules/User/Endpoints/ActivateUser.php
@@ -8,6 +8,7 @@ use App\Modules\User\Enums\UserState;
 use App\Modules\User\Events\UserActivated;
 use App\Modules\User\Requests\ActivateUserRequest;
 use App\Modules\User\User;
+use Illuminate\Support\Str;
 
 class ActivateUser extends Endpoint
 {
@@ -22,7 +23,7 @@ class ActivateUser extends Endpoint
         }
 
         $referrer = User::query()
-            ->where('referral_code', $request->input('referral_code'))
+            ->where('referral_code', Str::lower($request->input('referral_code')))
             ->where('state', UserState::ACTIVATED)
             ->first();
 
