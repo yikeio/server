b585d186f0ed337493d2223f72e186dc33bb1b88
ranpro <srizym@gmail.com>
2023-04-22 11:47:51 +0800

    feat: Google OAuth

--- app/Modules/Auth/Tests/CreateTokenViaCodeTest.php
+++ app/Modules/Auth/Tests/CreateTokenViaCodeTest.php
@@ -11,6 +11,46 @@ use Tests\TestCase;
 
 class CreateTokenViaCodeTest extends TestCase
 {
+    public function test_create_token_via_google_code()
+    {
+        $this->get('/api/auth/redirect?driver=google')->assertRedirect();
+
+        $this->mock(StateManager::class, function (MockInterface $mock) {
+            $mock->shouldReceive('get')->andReturn('google');
+        });
+
+        $this->mock(ProviderInterface::class, function (MockInterface $mock) {
+            $mock->shouldReceive('userFromCode')->andReturn(new User([
+                'id' => '123456',
+                'nickname' => 'test',
+                'name' => 'test',
+                'email' => null,
+                'avatar' => 'https://avatars.githubusercontent.com/u/123456?v=4',
+            ]));
+        });
+
+        $this->mock(SocialiteManager::class, function (MockInterface $mock) {
+            $mock->shouldReceive('create')->andReturn($this->app->make(ProviderInterface::class));
+        });
+
+        $this->assertDatabaseCount('users', 0);
+        $this->assertDatabaseCount('profiles', 0);
+
+        $this->postJson('/api/auth/tokens:via-code', [
+            'code' => '123456',
+            'state' => sha1('google'),
+        ])
+            ->assertSuccessful()
+            ->assertJsonStructure([
+                'value',
+                'type',
+                'expires_at',
+            ]);
+
+        $this->assertDatabaseCount('users', 1);
+        $this->assertDatabaseCount('profiles', 1);
+    }
+
     public function test_create_token_via_github_code()
     {
         $this->get('/api/auth/redirect?driver=github')->assertRedirect();
--- config/socialite.php
+++ config/socialite.php
@@ -6,4 +6,10 @@ return [
         'client_secret' => env('GITHUB_CLIENT_SECRET'),
         'redirect' => env('GITHUB_REDIRECT_URI'),
     ],
+
+    'google' => [
+        'client_id' => env('GOOGLE_CLIENT_ID'),
+        'client_secret' => env('GOOGLE_CLIENT_SECRET'),
+        'redirect' => env('GOOGLE_REDIRECT_URI'),
+    ],
 ];
